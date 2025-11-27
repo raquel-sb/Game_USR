5793097d7cf1d4899f42a5977dc618a3539aa1b5
Raquel Sans Barragán <raquel.sans01@estudiant.upf.edu>
2025-11-27 18:08:49 +0100

    Update folders organization + Mila movement working
    - Sandbox folders
    - Test folder
    - System folders
    
    - Task: Mila movement (8 directions for running + idle) working correctly, plus clean code

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # Game_USR
+
+## Estructura de las carpetas
+|-- Assets/
+|	|-- Audio/
+|	|-- Characters/
+|	|-- Environments/
+|	|-- Minigames/
+|	|-- Props/
+|	|-- Ui/
+|	|	|-- Images/
+|	|	|-- Menus/
+
+|-- Scenes/
+|	|-- Characters/
+|	|-- Cinematics/
+|	|-- Environments/
+|	|-- Gameplay/
+|	|-- Ui/
+|	|	|-- Menus/
+
+|-- Scripts/
+|	|-- Characters/
+|	|-- Systems/
+|	|-- Ui/
+
+|-- Sandbox/
+|	|-- Test_assets/
+|	|-- Test_scenes/
+
+|-- Tests/
+|	|-- characters/
+			test_character_movement.tscn
+|	|-- Systems/
+			test_inventory.tscn
+			test_scene_transition.tscn
+|	|-- Ui/
+			test_dialogue_ui.tscn
+|	|-- Minigames/
+			test_laundry_mechanic.tscn
+
+
+### Info específica de la estructura
+- Ui: hace referencia a menús, user interface, botones, HUD, etc
+- Scenes/Gameplay: scenes como Minijuegos, etc
+- Scenes/Environments: los escenarios jugables (en nuestro caso también podríamos llamarlo Environments)
+- Scenes/Cinematics: para guardar las cinemáticas "independientes" (algunas cinemáticas concretas pueden ir atadas a scenes concretas)
+- Assets/Props: elementos interactuables (objetos, etc)
+- Scripts/Systems: para gestión de inventario, cargado de escenas, gestión de variables, gestión de diálogos, guardado/cargado...
+- Shaders: shaders 2D simples como brillos, highlight al pasar el ratón...
+- Data: Diálogos en JSON, configuración de minijuegos, traducciones, información de objetos (como descripciones)
+- Autoload: son scripts de sistemas pero enfocados a aquellos que sean Managers, siempre accesibles, no se destruyen nunca (hasta que se cierre
+el programa) y son accesibles desde cualquier script (son globales, como singletones). Ej: GameState.gd, SceneManager.gd, AudioManager.gd, 
+DialogueManager.gd, Inventory.gd, SceneController.gd, NarrativeManager.gd, GlobalState.gd
+- Sandbox: carpeta donde guardar assets y scenes de prototipado y/o prueba. Por ejemplo, para testear mecánicas y demás
+- Tests: son escenas de prueba para poder testear los diferentes elementos del videojuego de forma aislada. NO son lo mismo que las pruebas
+de sandbox (donde suelen existir más al principio del desarrollo). Los tests son importantes de conservar, actualizar e ir creando más según
+necesidad y sistemas desarrollados
+
+
+### NOTAS Y DUDAS
+- Crear MenúManager.gd? Se pondría en carpeta Autoload y serviría para la navegación entre menús.
+- GameState.gd: Para flags de decisiones, objetos recogidos, minijuegos activos / inactivos (?), ...
---
+++ Sandbox/sb_mila_character.gd
@@ -0,0 +1,143 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+@export var speed := 300.0
+
+var movement_actions = ["ui_up", "ui_right", "ui_down", "ui_left"]
+var correct_mov_combinations = ["up", "down", "right", "left", "up_right", "up_left", "down_right", "down_left"]
+var last_direction = "down"
+var last_sprite = "idle"
+var move_dir = Vector2.ZERO
+
+func update_movement():
+	var input := Vector2.ZERO
+	
+	if last_sprite == "run":
+		if last_direction.contains("right"):
+			input.x += 1
+		if last_direction.contains("left"):
+			input.x -= 1
+		if last_direction.contains("down"):
+			input.y += 1
+		if last_direction.contains("up"):
+			input.y -= 1
+
+	# Normalizar si se está moviendo
+	if input != Vector2.ZERO:
+		input = input.normalized()
+
+	# Convertir entrada cartesiana → dirección isométrica
+	move_dir = cartesian_to_iso(input)
+	
+	# Actualizamos velocidad
+	velocity = move_dir.normalized() * speed
+
+
+func cartesian_to_iso(v: Vector2) -> Vector2:
+	# Conversión estándar 2:1
+	# Dependiendo del estilo de tu mapa puedes invertir X o Y.
+	# Esta es la clásica:
+	return Vector2(v.x - v.y, (v.x + v.y) * 0.5)
+
+func update_position():
+	move_and_slide() # Godot uses automatically velocity to update
+
+
+# Update functions
+func update_last_sprite(sprite_name):
+	last_sprite = sprite_name
+	
+func update_last_direction(direction_name):
+	last_direction = direction_name
+
+func play_animated_sprite(): # Function to update the sprite animation
+	var sprite_name = last_sprite + "_" + last_direction
+	_animated_sprite.play(sprite_name)
+
+func update_animation():
+	# Play the animtaion of the sprite (saved in the global variables of this script)
+	play_animated_sprite()
+
+
+# Function to get all currently pressed actions
+func get_pressed_actions() -> Array:
+	var pressed_actions: Array = []
+	
+	# Iterate over all actions defined in the InputMap
+	for action_name in movement_actions: # InputMap.get_actions():
+		if Input.is_action_pressed(action_name):
+			pressed_actions.append(action_name)
+	return pressed_actions
+
+
+# Function to identify if both arrays contain the same values
+func compare_arrays(arr1, arr2) -> bool:
+	if arr1.size() == arr2.size():
+		for val1 in arr1:
+			if val1 not in arr2:
+				return false
+		return true
+	else:
+		return false
+
+
+# Function to create the array for ui movement inputs given string of directions
+func split_mov_array(arr) -> Array:
+	var aux_arr = arr.split("_")
+	var prefix = "ui_"
+	var output_arr = []
+	
+	for value in aux_arr:
+		output_arr.append(prefix + value)
+	
+	return output_arr
+
+
+# Function that looks for the correct direction and sprite
+func update_direction_and_sprite():
+	var mov_actions_pressed = get_pressed_actions()
+	var mov_match = false
+	var i = 0
+	var str_mov : String
+	var ui_arr_mov : Array
+	
+	# If there exists a keyboard input, check if there is character movement involved
+	if mov_actions_pressed.size() > 0:
+
+		while i < correct_mov_combinations.size() and not mov_match:
+			# Generate the correct array of movements
+			str_mov = correct_mov_combinations[i]
+			ui_arr_mov = split_mov_array(str_mov)
+			
+			# Check if pressed actions are valid
+			mov_match = compare_arrays(mov_actions_pressed, ui_arr_mov)
+			
+			# Iterate index
+			i += 1
+	
+	# Update movement in case is a valid one
+	if mov_match:
+		# Update movement and direction
+		update_last_sprite("run")
+		update_last_direction(str_mov)
+		# last_sprite = "run"
+		# last_direction = str_mov
+
+	else:
+		update_last_sprite("idle")
+		# last_sprite = "idle"
+
+
+
+
+
+
+func _process(_delta):
+	update_direction_and_sprite()
+	update_animation()
+
+
+func _physics_process(delta):
+	update_movement()
+	update_position()
+	# handle_animation()
